3906f3ee4518dcc9d6a456b5a0f4cb8d319f4fcd
maguileran <31416999+maguileran@users.noreply.github.com>
2024-11-22 12:43:49 -0300

    feat: add netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -19,6 +20,7 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
+    netlifyPlugin(),
     tsconfigPaths(),
   ],
 });
